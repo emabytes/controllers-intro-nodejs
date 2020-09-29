b3698baeeeab6f782bb8926af2eb0f514e18828c
emabytes <emadavor22@gmail.com>
2020-09-29 23:38:43 +0200

    initial

---
+++ controllers/aboutController.js
@@ -0,0 +1,7 @@
+const about_Index = (req, res) => {
+    res.render('about')
+}
+
+module.exports = {
+    about_Index
+}
---
+++ controllers/indexController.js
@@ -0,0 +1,7 @@
+const index_Index = (req, res) => {
+    res.render("index")
+}
+
+module.exports = {
+    index_Index
+}
